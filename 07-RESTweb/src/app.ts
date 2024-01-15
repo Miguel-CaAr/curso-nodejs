2c90a4593d9856db0c87298ec552c0bc0a97fd1b
Miguel-CaAr <castroarredondo.hector@gmail.com>
2024-01-15 11:57:30 -0700

    seccion 08 end

--- 07-RESTweb/src/app.ts
+++ 07-RESTweb/src/app.ts
@@ -1,36 +1,15 @@
-import fs from "fs";
-import http2 from "http2";
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
 
-const server = http2.createSecureServer(
-  {
-    key: fs.readFileSync("keys/server.key"),
-    cert: fs.readFileSync("keys/server.crt"),
-  },
-  (req, res) => {
-    console.log(req.url);
-    if (req.url === "/") {
-      const htmlFile = fs.readFileSync("src/public/index.html", "utf-8");
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(htmlFile);
-      return;
-    }
+(async () => {
+  main();
+})();
 
-    // if (req.url?.endsWith(".js")) {
-    //   res.writeHead(200, { "Content-Type": "application/javascript" });
-    // } else if (req.url?.endsWith(".css")) {
-    //   res.writeHead(200, { "Content-Type": "text/css" });
-    // }
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH,
+  });
 
-    try {
-      const responseContent = fs.readFileSync(`src/public${req.url}`, `utf-8`);
-      res.end(responseContent);
-    } catch (error) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end();
-    }
-  }
-);
-
-server.listen(2121, () => {
-  console.log("Server running on port 8080");
-});
+  server.start();
+}
