1e4a3d8d8e6735593bace5b57fe2479f993950b2
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-03-03 16:48:30 -0700

    server http con node

---
+++ node-js-exercises/http.js
@@ -0,0 +1,14 @@
+const http = require("node:http");
+
+//Se crea una instancia de servidor usando el modulo http de node con la funcion 'createServer' que
+//  recibe una funcion que toma dos argumentos, la request y el response
+const server = http.createServer((req, res) => {
+  console.log("Request recibida");
+  res.end("Hola!");
+});
+//Se usa la instancia de servidor y su metodo listen para escuchar un puerto, en el primer argumento
+//  se define el puerto que en este caso es el 0 (el puerto 0 busca un puerto disponible y lo utiliza)
+//  se usa la funcion 'address()' para obtener el objeto y su propiedad 'port' para el puerto
+server.listen(0, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${server.address().port}`);
+});
