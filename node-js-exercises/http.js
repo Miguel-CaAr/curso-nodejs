ab13acbf5fb487e4b4dfe9c59422fe1840bc17de
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-03-09 02:25:55 -0700

    const desiredPort = process.env.PORT ?? 3000;

--- node-js-exercises/http.js
+++ node-js-exercises/http.js
@@ -1,6 +1,8 @@
 const http = require("node:http");
 const { findAvailablePort } = require("./freePort.js");
 
+const desiredPort = process.env.PORT ?? 3000;
+
 //Se crea una instancia de servidor usando el modulo http de node con la funcion 'createServer' que
 //  recibe una funcion que toma dos argumentos, la request y el response
 const server = http.createServer((req, res) => {
@@ -10,7 +12,7 @@ const server = http.createServer((req, res) => {
 //Se usa la instancia de servidor y su metodo listen para escuchar un puerto, en el primer argumento
 //  se define el puerto que en este caso es el 0 (el puerto 0 busca un puerto disponible y lo utiliza)
 //  se usa la funcion 'address()' para obtener el objeto y su propiedad 'port' para el puerto
-findAvailablePort(3000).then((port) => {
+findAvailablePort(desiredPort).then((port) => {
   server.listen(port, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
   });
